a1681442477eb82b5ed09e3107dd523a611e992e
wangxiaobo <2078350663@qq.com>
2023-12-05 18:06:23 +0800

    add  user and update views

--- Gemfile
+++ Gemfile
@@ -77,3 +77,5 @@ end
 
 gem "sidekiq", "~> 7.1"
 
+
+gem "devise", "~> 4.9"
---
+++ app/models/user.rb
@@ -0,0 +1,56 @@
+# == Schema Information
+# Schema version: 20231205094701
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  username               :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, authentication_keys: [:login]
+
+  validates :email, uniqueness: true
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validate :validate_username
+  attr_writer :login
+
+  def login
+    @login || username || email
+  end
+
+  def validate_username
+    return unless User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    
+  end
+
+  def downcase_fields
+    username.downcase!
+    email.downcase!
+  end
+
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(["username = :value OR email = :value", { value: login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+end
